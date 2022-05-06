a1b569eb0fac3add820e5d9dc9af7bd3f99f475a
Caio Henrique <caiohenriquela@outlook.com>
2022-05-05 21:38:47 -0300

    Continuando Testes

--- AluraflixTestes/VideoControllerTests.cs
+++ AluraflixTestes/VideoControllerTests.cs
@@ -1,8 +1,10 @@
 ﻿using Aluraflix.Controllers;
 using Aluraflix.Data.Dtos;
+using Aluraflix.Models;
 using Aluraflix.Profiles;
 using Aluraflix.Services;
 using AutoMapper;
+using FluentResults;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -31,13 +33,48 @@ namespace AluraflixTestes
         }
 
         [Fact]
+        public async Task AdicionaVideo()
+        {
+            //Arrange
+            var videoDto = new CreateVideoDto();
+            videoDto.Titulo = "VideoTeste3";
+            videoDto.Descricao = "DescriçãoVideoTeste3";
+            videoDto.CategoriaId = 20;
+
+            //Act
+            var readVideo = await _videoService.AdicionaVideo(videoDto);
+
+            //Assert
+            Assert.IsType<ReadVideoDto>(readVideo);
+        }
+
+        [Fact]
         public async Task RecuperarVideosPorTitulo()
         {
             //Act
             var videos = await _videoService.RecuperaVideoPorTitulo("");
+
             //Assert
-            Assert.Equal(2, videos.Count());
+            Assert.Equal(3, videos.Count());
         }
+
+        [Fact]
+        public async Task RecuperaVideoPorId()
+        {
+            //Act
+            var videos = await _videoService.RecuperaVideoPorId(1);
+
+            //Assert
+            Assert.NotNull(videos);
+        }
+
+        [Fact]
+        public async Task AtualizaVideo()
+        {
+            //TODO
+        }
+
+      
 
 
         public void Dispose()
