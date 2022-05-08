8d8cb1bb36c6e761162ef819eb67019cd9c4de9e
Caio Henrique <caiohenriquela@outlook.com>
2022-05-08 15:50:41 -0300

    Usuario API

--- AluraflixTestes/CategoriaServiceTests.cs
+++ AluraflixTestes/CategoriaServiceTests.cs
@@ -3,10 +3,7 @@ using Aluraflix.Profiles;
 using Aluraflix.Services;
 using AutoMapper;
 using FluentResults;
-using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using Xunit;
 
