6120ac437d6c4c87e62ac9d29dd54cf6812e2bc5
Caio Henrique <caiohenriquela@outlook.com>
2022-05-08 16:16:08 -0300

    Conectando usuario ao banco

--- UsuariosApi/Data/UserDbContext.cs
+++ UsuariosApi/Data/UserDbContext.cs
@@ -11,5 +11,41 @@ namespace UsuariosApi.Data
 
         }
 
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.Entity<IdentityUser<int>>(entity =>
+            {
+                entity.Property(m => m.Id).HasMaxLength(110);
+                entity.Property(m => m.Email).HasMaxLength(130);
+                entity.Property(m => m.NormalizedEmail).HasMaxLength(130);
+                entity.Property(m => m.NormalizedUserName).HasMaxLength(130);
+                entity.Property(m => m.UserName).HasMaxLength(130);
+            });
+            builder.Entity<IdentityRole<int>>(entity =>
+            {
+                entity.Property(m => m.Id).HasMaxLength(200);
+                entity.Property(m => m.Name).HasMaxLength(130);
+                entity.Property(m => m.NormalizedName).HasMaxLength(130);
+            });
+            builder.Entity<IdentityUserLogin<int>>(entity =>
+            {
+                entity.Property(m => m.LoginProvider).HasMaxLength(130);
+                entity.Property(m => m.ProviderKey).HasMaxLength(130);
+            });
+            builder.Entity<IdentityUserRole<int>>(entity =>
+            {
+                entity.Property(m => m.UserId).HasMaxLength(130);
+                entity.Property(m => m.RoleId).HasMaxLength(130);
+            });
+            builder.Entity<IdentityUserToken<int>>(entity =>
+            {
+                entity.Property(m => m.UserId).HasMaxLength(110);
+                entity.Property(m => m.LoginProvider).HasMaxLength(110);
+                entity.Property(m => m.Name).HasMaxLength(110);
+
+            });
+        }
+
     }
 }
